4f81040730e12f8609c5a2adfc0eb901dcc557f4
groverveizan <43590005+groverveizan@users.noreply.github.com>
2019-02-21 18:16:51 -0400

    inicio

--- WumpusWorld/WumpusWorld/Form1.Designer.cs
+++ WumpusWorld/WumpusWorld/Form1.Designer.cs
@@ -30,73 +30,73 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnIzquierda = new System.Windows.Forms.Button();
+            this.btnArriba = new System.Windows.Forms.Button();
+            this.btnDerecha = new System.Windows.Forms.Button();
+            this.btnAbajo = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flechasGroup = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, -18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Wumpus World";
             // 
-            // button2
+            // btnIzquierda
             // 
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Location = new System.Drawing.Point(123, 850);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 70);
-            this.button2.TabIndex = 2;
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnIzquierda.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnIzquierda.BackgroundImage")));
+            this.btnIzquierda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnIzquierda.Location = new System.Drawing.Point(674, 129);
+            this.btnIzquierda.Name = "btnIzquierda";
+            this.btnIzquierda.Size = new System.Drawing.Size(70, 70);
+            this.btnIzquierda.TabIndex = 2;
+            this.btnIzquierda.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnArriba
             // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Location = new System.Drawing.Point(191, 783);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(70, 70);
-            this.button1.TabIndex = 3;
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnArriba.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnArriba.BackgroundImage")));
+            this.btnArriba.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnArriba.Location = new System.Drawing.Point(742, 63);
+            this.btnArriba.Name = "btnArriba";
+            this.btnArriba.Size = new System.Drawing.Size(70, 70);
+            this.btnArriba.TabIndex = 3;
+            this.btnArriba.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnDerecha
             // 
-            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Location = new System.Drawing.Point(258, 850);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(70, 70);
-            this.button3.TabIndex = 4;
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnDerecha.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDerecha.BackgroundImage")));
+            this.btnDerecha.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDerecha.Location = new System.Drawing.Point(809, 128);
+            this.btnDerecha.Name = "btnDerecha";
+            this.btnDerecha.Size = new System.Drawing.Size(70, 70);
+            this.btnDerecha.TabIndex = 4;
+            this.btnDerecha.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnAbajo
             // 
-            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button4.Location = new System.Drawing.Point(191, 918);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(70, 70);
-            this.button4.TabIndex = 5;
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnAbajo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAbajo.BackgroundImage")));
+            this.btnAbajo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAbajo.Location = new System.Drawing.Point(742, 195);
+            this.btnAbajo.Name = "btnAbajo";
+            this.btnAbajo.Size = new System.Drawing.Size(70, 70);
+            this.btnAbajo.TabIndex = 5;
+            this.btnAbajo.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
@@ -106,12 +106,52 @@
             this.groupBox1.Controls.Add(this.pictureBox2);
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(1211, 26);
+            this.groupBox1.Location = new System.Drawing.Point(1118, 11);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(189, 743);
+            this.groupBox1.Size = new System.Drawing.Size(225, 743);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Sensores";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(36, 559);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(141, 121);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(36, 420);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(141, 122);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(36, 286);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(141, 115);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(36, 161);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(141, 110);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
@@ -119,56 +159,16 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(36, 44);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.Size = new System.Drawing.Size(141, 101);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(36, 184);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
-            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(36, 323);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
-            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(36, 459);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
-            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox5.Location = new System.Drawing.Point(36, 597);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox5.TabIndex = 4;
-            this.pictureBox5.TabStop = false;
             // 
             // flechasGroup
             // 
             this.flechasGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.flechasGroup.Location = new System.Drawing.Point(846, 812);
+            this.flechasGroup.Location = new System.Drawing.Point(708, 431);
             this.flechasGroup.Name = "flechasGroup";
-            this.flechasGroup.Size = new System.Drawing.Size(554, 155);
+            this.flechasGroup.Size = new System.Drawing.Size(404, 260);
             this.flechasGroup.TabIndex = 7;
             this.flechasGroup.TabStop = false;
             this.flechasGroup.Text = "Flechas";
@@ -177,24 +177,24 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1448, 1061);
+            this.ClientSize = new System.Drawing.Size(1370, 749);
             this.Controls.Add(this.flechasGroup);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAbajo);
+            this.Controls.Add(this.btnDerecha);
+            this.Controls.Add(this.btnArriba);
+            this.Controls.Add(this.btnIzquierda);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -203,10 +203,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnIzquierda;
+        private System.Windows.Forms.Button btnArriba;
+        private System.Windows.Forms.Button btnDerecha;
+        private System.Windows.Forms.Button btnAbajo;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox4;
--- WumpusWorld/WumpusWorld/Form1.cs
+++ WumpusWorld/WumpusWorld/Form1.cs
@@ -13,15 +13,67 @@ namespace WumpusWorld
     public partial class Form1 : Form
     {
         //Cueva de Picture Box Generada
-        PictureBox[,] cueva = new PictureBox[4,4];
+        PictureBox[,] cueva = new PictureBox[4, 4];
+        bool[,] posJugador = new bool[4, 4];
+        bool[,] posWumpus = new bool[4, 4];
+        bool[,] posPuerta = new bool[4, 4];
+        bool[,] posHoyos = new bool[4, 4];
+        bool[,] posOro = new bool[4, 4];
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        private int NumeroRandomico()
+        {
+            Random genRandom = new Random();
+            int numero = genRandom.Next(0, 3);
+            return numero;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
+            //generar jugador
+            posJugador[0, 0] = true;
+            
+            //Generar Wumpus
+            int posX_Wumpus;
+            int posY_Wumpus; 
+            do
+            {
+                posX_Wumpus = NumeroRandomico();
+                posY_Wumpus = NumeroRandomico();
+            }
+            while ((posX_Wumpus == 0 && posY_Wumpus == 0) || (posX_Wumpus == 1 && posY_Wumpus == 0) || (posX_Wumpus == 0 && posY_Wumpus == 1));
+            posWumpus[posX_Wumpus, posY_Wumpus] = true;
+
+            //Generar Oro
+            int posX_Oro;
+            int posY_Oro;
+            do
+            {
+                posX_Oro = NumeroRandomico();
+                posY_Oro = NumeroRandomico();
+            }
+            while ((posX_Oro == 0 && posY_Oro == 0) || (posX_Oro == 1 && posY_Oro == 0) || (posX_Oro == 0 && posY_Oro == 1));
+            posOro[posX_Oro, posY_Oro] = true;
+
+            //Generar Hoyos
+            int canHoyosGen = 0;
+            while (canHoyosGen < 3)
+            {
+                int posX_Hoyo;
+                int posY_Hoyo;
+                do
+                {
+                    posX_Hoyo = NumeroRandomico();
+                    posY_Hoyo = NumeroRandomico();
+                }
+                while ((posX_Hoyo == 0 && posY_Hoyo == 0) || (posX_Hoyo == 1 && posY_Hoyo == 0) || (posX_Hoyo == 0 && posY_Hoyo == 1));
+                posOro[posX_Hoyo, posY_Hoyo] = true;
+                canHoyosGen++;
+            }
+
             for (int x = 0; x < cueva.GetLength(0); x++)
             {
                 for (int y = 0; y < cueva.GetLength(1); y++)
@@ -29,11 +81,30 @@ namespace WumpusWorld
                     cueva[x, y] = new PictureBox();
                     cueva[x, y].Size = new Size(150, 150);
                     cueva[x, y].BorderStyle = BorderStyle.FixedSingle;
-                    cueva[x, y].Location = new Point(250 + (x * 150), 150 + (y * 150));
+                    cueva[x, y].Location = new Point(50 + (x * 150), 50 + (y * 150));
+
+                    if (posJugador[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.BlueViolet;
+                    }
+                    else if (posWumpus[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.Red;
+                    }
+                    else if (posOro[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.Yellow;
+                    }
+                    else if (posHoyos[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.Black;
+                    }
 
                     this.Controls.Add(cueva[x, y]);
                 }
             }
         }
+
+
     }
 }
