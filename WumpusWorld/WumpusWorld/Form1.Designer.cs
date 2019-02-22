4aeb56fadf5370d801c9776f993392918c6e23e6
groverveizan <43590005+groverveizan@users.noreply.github.com>
2019-02-22 18:17:24 -0400

    Avance Viernes

--- WumpusWorld/WumpusWorld/Form1.Designer.cs
+++ WumpusWorld/WumpusWorld/Form1.Designer.cs
@@ -39,14 +39,24 @@
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.sensor = new System.Windows.Forms.PictureBox();
             this.flechasGroup = new System.Windows.Forms.GroupBox();
+            this.brisaDetector = new System.Windows.Forms.PictureBox();
+            this.hedorDetector = new System.Windows.Forms.PictureBox();
+            this.resplandorDetector = new System.Windows.Forms.PictureBox();
+            this.luzDetector = new System.Windows.Forms.PictureBox();
+            this.gritoDetector = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sensor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brisaDetector)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hedorDetector)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resplandorDetector)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.luzDetector)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gritoDetector)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -67,6 +77,7 @@
             this.btnIzquierda.Size = new System.Drawing.Size(70, 70);
             this.btnIzquierda.TabIndex = 2;
             this.btnIzquierda.UseVisualStyleBackColor = true;
+            this.btnIzquierda.Click += new System.EventHandler(this.btnIzquierda_Click);
             // 
             // btnArriba
             // 
@@ -77,6 +88,7 @@
             this.btnArriba.Size = new System.Drawing.Size(70, 70);
             this.btnArriba.TabIndex = 3;
             this.btnArriba.UseVisualStyleBackColor = true;
+            this.btnArriba.Click += new System.EventHandler(this.btnArriba_Click);
             // 
             // btnDerecha
             // 
@@ -87,6 +99,7 @@
             this.btnDerecha.Size = new System.Drawing.Size(70, 70);
             this.btnDerecha.TabIndex = 4;
             this.btnDerecha.UseVisualStyleBackColor = true;
+            this.btnDerecha.Click += new System.EventHandler(this.btnDerecha_Click);
             // 
             // btnAbajo
             // 
@@ -97,14 +110,20 @@
             this.btnAbajo.Size = new System.Drawing.Size(70, 70);
             this.btnAbajo.TabIndex = 5;
             this.btnAbajo.UseVisualStyleBackColor = true;
+            this.btnAbajo.Click += new System.EventHandler(this.btnAbajo_Click);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.gritoDetector);
+            this.groupBox1.Controls.Add(this.luzDetector);
+            this.groupBox1.Controls.Add(this.resplandorDetector);
+            this.groupBox1.Controls.Add(this.hedorDetector);
+            this.groupBox1.Controls.Add(this.brisaDetector);
             this.groupBox1.Controls.Add(this.pictureBox5);
             this.groupBox1.Controls.Add(this.pictureBox4);
             this.groupBox1.Controls.Add(this.pictureBox3);
             this.groupBox1.Controls.Add(this.pictureBox2);
-            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.sensor);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(1118, 11);
             this.groupBox1.Name = "groupBox1";
@@ -117,7 +136,7 @@
             // 
             this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
             this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox5.Location = new System.Drawing.Point(36, 559);
+            this.pictureBox5.Location = new System.Drawing.Point(73, 559);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(141, 121);
             this.pictureBox5.TabIndex = 4;
@@ -127,7 +146,7 @@
             // 
             this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
             this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(36, 420);
+            this.pictureBox4.Location = new System.Drawing.Point(73, 420);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(141, 122);
             this.pictureBox4.TabIndex = 3;
@@ -137,7 +156,7 @@
             // 
             this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(36, 286);
+            this.pictureBox3.Location = new System.Drawing.Point(73, 286);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(141, 115);
             this.pictureBox3.TabIndex = 2;
@@ -147,21 +166,21 @@
             // 
             this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(36, 161);
+            this.pictureBox2.Location = new System.Drawing.Point(73, 161);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(141, 110);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox1
+            // sensor
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(36, 44);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(141, 101);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.sensor.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("sensor.BackgroundImage")));
+            this.sensor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.sensor.Location = new System.Drawing.Point(73, 44);
+            this.sensor.Name = "sensor";
+            this.sensor.Size = new System.Drawing.Size(141, 101);
+            this.sensor.TabIndex = 0;
+            this.sensor.TabStop = false;
             // 
             // flechasGroup
             // 
@@ -172,6 +191,46 @@
             this.flechasGroup.TabIndex = 7;
             this.flechasGroup.TabStop = false;
             this.flechasGroup.Text = "Flechas";
+            // 
+            // brisaDetector
+            // 
+            this.brisaDetector.Location = new System.Drawing.Point(23, 82);
+            this.brisaDetector.Name = "brisaDetector";
+            this.brisaDetector.Size = new System.Drawing.Size(27, 27);
+            this.brisaDetector.TabIndex = 5;
+            this.brisaDetector.TabStop = false;
+            // 
+            // hedorDetector
+            // 
+            this.hedorDetector.Location = new System.Drawing.Point(23, 204);
+            this.hedorDetector.Name = "hedorDetector";
+            this.hedorDetector.Size = new System.Drawing.Size(27, 27);
+            this.hedorDetector.TabIndex = 6;
+            this.hedorDetector.TabStop = false;
+            // 
+            // resplandorDetector
+            // 
+            this.resplandorDetector.Location = new System.Drawing.Point(23, 333);
+            this.resplandorDetector.Name = "resplandorDetector";
+            this.resplandorDetector.Size = new System.Drawing.Size(27, 27);
+            this.resplandorDetector.TabIndex = 8;
+            this.resplandorDetector.TabStop = false;
+            // 
+            // luzDetector
+            // 
+            this.luzDetector.Location = new System.Drawing.Point(23, 470);
+            this.luzDetector.Name = "luzDetector";
+            this.luzDetector.Size = new System.Drawing.Size(27, 27);
+            this.luzDetector.TabIndex = 8;
+            this.luzDetector.TabStop = false;
+            // 
+            // gritoDetector
+            // 
+            this.gritoDetector.Location = new System.Drawing.Point(23, 609);
+            this.gritoDetector.Name = "gritoDetector";
+            this.gritoDetector.Size = new System.Drawing.Size(27, 27);
+            this.gritoDetector.TabIndex = 8;
+            this.gritoDetector.TabStop = false;
             // 
             // Form1
             // 
@@ -194,7 +253,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sensor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brisaDetector)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hedorDetector)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resplandorDetector)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.luzDetector)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gritoDetector)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,8 +276,13 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox sensor;
         private System.Windows.Forms.GroupBox flechasGroup;
+        private System.Windows.Forms.PictureBox gritoDetector;
+        private System.Windows.Forms.PictureBox luzDetector;
+        private System.Windows.Forms.PictureBox resplandorDetector;
+        private System.Windows.Forms.PictureBox hedorDetector;
+        private System.Windows.Forms.PictureBox brisaDetector;
     }
 }
 
--- WumpusWorld/WumpusWorld/Form1.cs
+++ WumpusWorld/WumpusWorld/Form1.cs
@@ -12,13 +12,29 @@ namespace WumpusWorld
 {
     public partial class Form1 : Form
     {
+        //Timer
+        Timer timer = new Timer();
+
         //Cueva de Picture Box Generada
         PictureBox[,] cueva = new PictureBox[4, 4];
+        //Generar Posicion
         bool[,] posJugador = new bool[4, 4];
         bool[,] posWumpus = new bool[4, 4];
-        bool[,] posPuerta = new bool[4, 4];
+        bool[,] posSalida = new bool[4, 4];
         bool[,] posHoyos = new bool[4, 4];
         bool[,] posOro = new bool[4, 4];
+        //Generar Sensores
+        bool[,] sensor_hedor = new bool[4, 4];
+        bool[,] sensor_brisa = new bool[4, 4];
+        bool[,] sensor_resplandor = new bool[4, 4];
+        bool[,] sensor_luz = new bool[4, 4];
+        bool[,] sensor_grito = new bool[4, 4];
+
+        int PosX_Actual_Jugador = 0;
+        int PosY_Actual_Jugador = 0;
+
+        int cantFlechas = 1;
+        PictureBox[] flechas;
 
         public Form1()
         {
@@ -28,17 +44,17 @@ namespace WumpusWorld
         private int NumeroRandomico()
         {
             Random genRandom = new Random();
-            int numero = genRandom.Next(0, 3);
+            int numero = genRandom.Next(0, 4);
             return numero;
         }
         private void Form1_Load(object sender, EventArgs e)
         {
             //generar jugador
             posJugador[0, 0] = true;
-            
+
             //Generar Wumpus
             int posX_Wumpus;
-            int posY_Wumpus; 
+            int posY_Wumpus;
             do
             {
                 posX_Wumpus = NumeroRandomico();
@@ -55,12 +71,24 @@ namespace WumpusWorld
                 posX_Oro = NumeroRandomico();
                 posY_Oro = NumeroRandomico();
             }
-            while ((posX_Oro == 0 && posY_Oro == 0) || (posX_Oro == 1 && posY_Oro == 0) || (posX_Oro == 0 && posY_Oro == 1));
+            while ((posX_Oro == 0 && posY_Oro == 0) || (posX_Oro == 1 && posY_Oro == 0) || (posX_Oro == 0 && posY_Oro == 1) || (posX_Oro == posX_Wumpus && posY_Oro == posY_Wumpus));
             posOro[posX_Oro, posY_Oro] = true;
 
+            //Generar Oro
+            int posX_Salida;
+            int posY_Salida;
+            do
+            {
+                posX_Salida = NumeroRandomico();
+                posY_Salida = NumeroRandomico();
+            }
+            while ((posX_Salida == 0 && posY_Salida == 0) || (posX_Salida == 1 && posY_Salida == 0) || (posX_Salida == 0 && posY_Salida == 1) || (posX_Salida == posX_Wumpus && posY_Salida == posY_Wumpus) || (posX_Salida == posX_Oro && posY_Salida == posY_Oro) || (posX_Salida < 2 || posY_Salida < 2) || (posX_Salida == 2 && posY_Salida == 2));
+            posSalida[posX_Salida, posY_Salida] = true;
+
             //Generar Hoyos
-            int canHoyosGen = 0;
-            while (canHoyosGen < 3)
+            int posX_Hoyo_def = 0;
+            int posY_Hoyo_def = 0;
+            for (int cantHoyosGen = 0; cantHoyosGen < 2; cantHoyosGen++)
             {
                 int posX_Hoyo;
                 int posY_Hoyo;
@@ -69,9 +97,10 @@ namespace WumpusWorld
                     posX_Hoyo = NumeroRandomico();
                     posY_Hoyo = NumeroRandomico();
                 }
-                while ((posX_Hoyo == 0 && posY_Hoyo == 0) || (posX_Hoyo == 1 && posY_Hoyo == 0) || (posX_Hoyo == 0 && posY_Hoyo == 1));
-                posOro[posX_Hoyo, posY_Hoyo] = true;
-                canHoyosGen++;
+                while ((posX_Hoyo == 0 && posY_Hoyo == 0) || (posX_Hoyo == 1 && posY_Hoyo == 0) || (posX_Hoyo == 0 && posY_Hoyo == 1) || (posX_Hoyo == posX_Oro && posY_Hoyo == posY_Oro) || (posX_Hoyo == posX_Wumpus && posY_Hoyo == posY_Wumpus) || (posX_Hoyo == posX_Salida && posY_Hoyo == posY_Salida) || (posX_Hoyo == posX_Hoyo_def && posY_Hoyo == posY_Hoyo_def) || (posX_Hoyo > 2 || posY_Hoyo > 2) || ((posX_Hoyo == posX_Hoyo_def + 1 || posX_Hoyo == posX_Hoyo_def - 1) || (posY_Hoyo == posY_Hoyo_def + 1 || posY_Hoyo == posY_Hoyo_def - 1)));
+                posHoyos[posX_Hoyo, posY_Hoyo] = true;
+                posX_Hoyo_def = posX_Hoyo;
+                posY_Hoyo_def = posY_Hoyo;
             }
 
             for (int x = 0; x < cueva.GetLength(0); x++)
@@ -90,14 +119,138 @@ namespace WumpusWorld
                     else if (posWumpus[x, y])
                     {
                         cueva[x, y].BackColor = Color.Red;
+                        if (x + 1 <= 3)
+                            sensor_hedor[x + 1, y] = true;
+                        if (x - 1 >= 0)
+                            sensor_hedor[x - 1, y] = true;
+                        if (y + 1 <= 3)
+                            sensor_hedor[x, y + 1] = true;
+                        if (y - 1 >= 0)
+                            sensor_hedor[x, y - 1] = true;
                     }
                     else if (posOro[x, y])
                     {
                         cueva[x, y].BackColor = Color.Yellow;
+                        if (x + 1 <= 3)
+                            sensor_resplandor[x + 1, y] = true;
+                        if (x - 1 >= 0)
+                            sensor_resplandor[x - 1, y] = true;
+                        if (y + 1 <= 3)
+                            sensor_resplandor[x, y + 1] = true;
+                        if (y - 1 >= 0)
+                            sensor_resplandor[x, y - 1] = true;
+                    }
+                    else if (posSalida[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.Brown;
+                        if (x + 1 <= 3)
+                            sensor_luz[x + 1, y] = true;
+                        if (x - 1 >= 0)
+                            sensor_luz[x - 1, y] = true;
+                        if (y + 1 <= 3)
+                            sensor_luz[x, y + 1] = true;
+                        if (y - 1 >= 0)
+                            sensor_luz[x, y - 1] = true;
                     }
                     else if (posHoyos[x, y])
                     {
                         cueva[x, y].BackColor = Color.Black;
+                        if (x + 1 <= 3)
+                            sensor_brisa[x + 1, y] = true;
+                        if (x - 1 >= 0)
+                            sensor_brisa[x - 1, y] = true;
+                        if (y + 1 <= 3)
+                            sensor_brisa[x, y + 1] = true;
+                        if (y - 1 >= 0)
+                            sensor_brisa[x, y - 1] = true;
+                    }
+
+                    this.Controls.Add(cueva[x, y]);
+                }
+            }
+
+            flechas = new PictureBox[2];
+            flechas[0] = new PictureBox();
+            flechas[0].Size = new Size(150, 150);
+            flechas[0].Location = new Point(50, 50);
+            flechas[0].BackColor = Color.ForestGreen;
+            flechasGroup.Controls.Add(flechas[0]);
+
+            definirMovimiento();
+        }
+
+
+        private void definirMovimiento()
+        {
+            btnIzquierda.Visible = PosX_Actual_Jugador - 1 >= 0 ? true : false;
+            btnDerecha.Visible = PosX_Actual_Jugador + 1 <= 3 ? true : false;
+            btnArriba.Visible = PosY_Actual_Jugador - 1 >= 0 ? true : false;
+            btnAbajo.Visible = PosY_Actual_Jugador + 1 <= 3 ? true : false;
+        }
+
+        private void redefinirCueva()
+        {
+            for (int x = 0; x < cueva.GetLength(0); x++)
+            {
+                for (int y = 0; y < cueva.GetLength(1); y++)
+                {
+                    cueva[x, y].Dispose();
+                    cueva[x, y] = new PictureBox();
+                    cueva[x, y].Size = new Size(150, 150);
+                    cueva[x, y].BorderStyle = BorderStyle.FixedSingle;
+                    cueva[x, y].Location = new Point(50 + (x * 150), 50 + (y * 150));
+
+                    if (posJugador[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.BlueViolet;
+                    }
+                    else if (posWumpus[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.Red;
+                        if (x + 1 <= 3)
+                            sensor_hedor[x + 1, y] = true;
+                        if (x - 1 >= 0)
+                            sensor_hedor[x - 1, y] = true;
+                        if (y + 1 <= 3)
+                            sensor_hedor[x, y + 1] = true;
+                        if (y - 1 >= 0)
+                            sensor_hedor[x, y - 1] = true;
+                    }
+                    else if (posOro[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.Yellow;
+                        if (x + 1 <= 3)
+                            sensor_resplandor[x + 1, y] = true;
+                        if (x - 1 >= 0)
+                            sensor_resplandor[x - 1, y] = true;
+                        if (y + 1 <= 3)
+                            sensor_resplandor[x, y + 1] = true;
+                        if (y - 1 >= 0)
+                            sensor_resplandor[x, y - 1] = true;
+                    }
+                    else if (posSalida[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.Brown;
+                        if (x + 1 <= 3)
+                            sensor_luz[x + 1, y] = true;
+                        if (x - 1 >= 0)
+                            sensor_luz[x - 1, y] = true;
+                        if (y + 1 <= 3)
+                            sensor_luz[x, y + 1] = true;
+                        if (y - 1 >= 0)
+                            sensor_luz[x, y - 1] = true;
+                    }
+                    else if (posHoyos[x, y])
+                    {
+                        cueva[x, y].BackColor = Color.Black;
+                        if (x + 1 <= 3)
+                            sensor_brisa[x + 1, y] = true;
+                        if (x - 1 >= 0)
+                            sensor_brisa[x - 1, y] = true;
+                        if (y + 1 <= 3)
+                            sensor_brisa[x, y + 1] = true;
+                        if (y - 1 >= 0)
+                            sensor_brisa[x, y - 1] = true;
                     }
 
                     this.Controls.Add(cueva[x, y]);
@@ -105,6 +258,53 @@ namespace WumpusWorld
             }
         }
 
+        private void ActivarSensores()
+        {
+            brisaDetector.BackColor = sensor_brisa[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.Red : Control.DefaultBackColor;
+            hedorDetector.BackColor = sensor_hedor[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.Red : Control.DefaultBackColor;
+            resplandorDetector.BackColor = sensor_resplandor[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.Yellow : Control.DefaultBackColor;
+            gritoDetector.BackColor = sensor_grito[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.Green : Control.DefaultBackColor;
+            luzDetector.BackColor = sensor_luz[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.SkyBlue : Control.DefaultBackColor;
+        }
 
+        private void btnDerecha_Click(object sender, EventArgs e)
+        {
+            posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
+            PosX_Actual_Jugador = PosX_Actual_Jugador + 1;
+            posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
+            redefinirCueva();
+            definirMovimiento();
+            ActivarSensores();
+        }
+
+        private void btnIzquierda_Click(object sender, EventArgs e)
+        {
+            posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
+            PosX_Actual_Jugador = PosX_Actual_Jugador - 1;
+            posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
+            redefinirCueva();
+            definirMovimiento();
+            ActivarSensores();
+        }
+
+        private void btnArriba_Click(object sender, EventArgs e)
+        {
+            posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
+            PosY_Actual_Jugador = PosY_Actual_Jugador - 1;
+            posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
+            redefinirCueva();
+            definirMovimiento();
+            ActivarSensores();
+        }
+
+        private void btnAbajo_Click(object sender, EventArgs e)
+        {
+            posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
+            PosY_Actual_Jugador = PosY_Actual_Jugador + 1;
+            posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
+            redefinirCueva();
+            definirMovimiento();
+            ActivarSensores();
+        }
     }
 }
