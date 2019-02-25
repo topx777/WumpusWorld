0c22ed2dcc0915dce3554fc70eb1a3c168318510
Abel Lopez Paniagua <topx777@gmail.com>
2019-02-25 14:55:35 -0600

    Wumpus casi finalizado

--- WumpusWorld/WumpusWorld/Form1.cs
+++ WumpusWorld/WumpusWorld/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Resources;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,6 @@ namespace WumpusWorld
 {
     public partial class Form1 : Form
     {
-        //Timer
-        Timer timer = new Timer();
-
         //Cueva de Picture Box Generada
         PictureBox[,] cueva = new PictureBox[4, 4];
         //Generar Posicion
@@ -30,11 +29,16 @@ namespace WumpusWorld
         bool[,] sensor_luz = new bool[4, 4];
         bool[,] sensor_grito = new bool[4, 4];
 
+        //Matriz de Recorrido
+        bool[,] recorrido = new bool[4, 4];
+
         int PosX_Actual_Jugador = 0;
         int PosY_Actual_Jugador = 0;
 
         int cantFlechas = 1;
         PictureBox[] flechas;
+        bool flecha_cargada = false;
+
 
         public Form1()
         {
@@ -51,6 +55,7 @@ namespace WumpusWorld
         {
             //generar jugador
             posJugador[0, 0] = true;
+            recorrido[0, 0] = true;
 
             //Generar Wumpus
             int posX_Wumpus;
@@ -111,14 +116,16 @@ namespace WumpusWorld
                     cueva[x, y].Size = new Size(150, 150);
                     cueva[x, y].BorderStyle = BorderStyle.FixedSingle;
                     cueva[x, y].Location = new Point(50 + (x * 150), 50 + (y * 150));
+                    cueva[x, y].Tag = x + "," + y;
+                    cueva[x, y].Click += IntentarMatar;
 
                     if (posJugador[x, y])
                     {
-                        cueva[x, y].BackColor = Color.BlueViolet;
+                        cueva[x, y].BackgroundImage = WumpusWorld.ImagenesRes.jugador;
+                        cueva[x, y].BackgroundImageLayout = ImageLayout.Stretch;
                     }
                     else if (posWumpus[x, y])
                     {
-                        cueva[x, y].BackColor = Color.Red;
                         if (x + 1 <= 3)
                             sensor_hedor[x + 1, y] = true;
                         if (x - 1 >= 0)
@@ -130,7 +137,6 @@ namespace WumpusWorld
                     }
                     else if (posOro[x, y])
                     {
-                        cueva[x, y].BackColor = Color.Yellow;
                         if (x + 1 <= 3)
                             sensor_resplandor[x + 1, y] = true;
                         if (x - 1 >= 0)
@@ -142,7 +148,6 @@ namespace WumpusWorld
                     }
                     else if (posSalida[x, y])
                     {
-                        cueva[x, y].BackColor = Color.Brown;
                         if (x + 1 <= 3)
                             sensor_luz[x + 1, y] = true;
                         if (x - 1 >= 0)
@@ -154,7 +159,6 @@ namespace WumpusWorld
                     }
                     else if (posHoyos[x, y])
                     {
-                        cueva[x, y].BackColor = Color.Black;
                         if (x + 1 <= 3)
                             sensor_brisa[x + 1, y] = true;
                         if (x - 1 >= 0)
@@ -172,13 +176,73 @@ namespace WumpusWorld
             flechas = new PictureBox[2];
             flechas[0] = new PictureBox();
             flechas[0].Size = new Size(150, 150);
-            flechas[0].Location = new Point(50, 50);
-            flechas[0].BackColor = Color.ForestGreen;
+            flechas[0].Location = new Point(40, 50);
+            flechas[0].BackgroundImage = WumpusWorld.ImagenesRes.flecha;
+            flechas[0].BackgroundImageLayout = ImageLayout.Stretch;
+
+            flechas[0].Click += CargarArco;
+
             flechasGroup.Controls.Add(flechas[0]);
 
             definirMovimiento();
         }
 
+        private void IntentarMatar(object sender, EventArgs e)
+        {
+            if(flecha_cargada)
+            {
+                string posicion = ((PictureBox)sender).Tag.ToString();
+                int posXmatar = Convert.ToInt32(posicion[0].ToString());
+                int posYmatar = Convert.ToInt32(posicion[2].ToString());
+                if((posXmatar == PosX_Actual_Jugador + 1 && posYmatar == PosY_Actual_Jugador) || (posXmatar == PosX_Actual_Jugador + - 1 && posYmatar == PosY_Actual_Jugador) || (posYmatar == PosY_Actual_Jugador + 1 && posXmatar == PosX_Actual_Jugador) || (posYmatar == PosY_Actual_Jugador - 1 && posXmatar == PosX_Actual_Jugador))
+                {
+                    if(posWumpus[posXmatar, posYmatar])
+                    {
+                        MessageBox.Show("Wumpus asesinado");
+                        posWumpus[posXmatar, posYmatar] = false;
+                        if (posXmatar + 1 <= 3)
+                        {
+                            sensor_hedor[posXmatar + 1, posYmatar] = false;
+                            sensor_grito[posXmatar + 1, posYmatar] = true;
+                        }
+                        if (posXmatar - 1 >= 0)
+                        {
+                            sensor_hedor[posXmatar - 1, posYmatar] = false;
+                            sensor_grito[posXmatar - 1, posYmatar] = true;
+                        }
+                        if (posYmatar + 1 <= 3)
+                        {
+                            sensor_hedor[posXmatar, posYmatar + 1] = false;
+                            sensor_grito[posXmatar, posYmatar + 1] = true;
+                        }
+                        if (posYmatar - 1 >= 0)
+                        {
+                            sensor_hedor[posXmatar, posYmatar - 1] = false;
+                            sensor_grito[posXmatar, posYmatar - 1] = true;
+                        }
+                        gritoDetector.BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Parece, que no habia nada en ese cuarto, flecha malgastada");
+                    }
+                    flecha_cargada = false;
+                }
+                else
+                {
+                    MessageBox.Show("Estas muy lejos para dispara ahi");
+                }
+            }
+        }
+
+        private void CargarArco(object sender, EventArgs e)
+        {
+            if (cantFlechas > 0 && !flecha_cargada) {
+                flecha_cargada = true;
+                cantFlechas--;
+                flechasGroup.Controls.RemoveAt(cantFlechas);
+            }
+        }
 
         private void definirMovimiento()
         {
@@ -199,59 +263,49 @@ namespace WumpusWorld
                     cueva[x, y].Size = new Size(150, 150);
                     cueva[x, y].BorderStyle = BorderStyle.FixedSingle;
                     cueva[x, y].Location = new Point(50 + (x * 150), 50 + (y * 150));
+                    cueva[x, y].Tag = x + "," + y;
+                    cueva[x, y].Click += IntentarMatar;
+
 
                     if (posJugador[x, y])
                     {
-                        cueva[x, y].BackColor = Color.BlueViolet;
+                        cueva[x, y].BackgroundImage = WumpusWorld.ImagenesRes.jugador;
+                        cueva[x, y].BackgroundImageLayout = ImageLayout.Stretch;
                     }
-                    else if (posWumpus[x, y])
+
+                    if(recorrido[x, y] && !posJugador[x, y])
                     {
-                        cueva[x, y].BackColor = Color.Red;
-                        if (x + 1 <= 3)
-                            sensor_hedor[x + 1, y] = true;
-                        if (x - 1 >= 0)
-                            sensor_hedor[x - 1, y] = true;
-                        if (y + 1 <= 3)
-                            sensor_hedor[x, y + 1] = true;
-                        if (y - 1 >= 0)
-                            sensor_hedor[x, y - 1] = true;
+                        int cantSensores = 0;
+                        string sensor_query = "";
+                        if (sensor_brisa[x, y])
+                        {
+                            cantSensores++;
+                            sensor_query = sensor_query + "_Brisa";
+                        }
+                        if (sensor_hedor[x, y])
+                        {
+                            cantSensores++;
+                            sensor_query = sensor_query + "_Hedor";
+                        }
+                        if (sensor_resplandor[x, y])
+                        {
+                            cantSensores++;
+                            sensor_query = sensor_query + "_Brillo";
+                        }
+                        if (sensor_luz[x, y])
+                        {
+                            cantSensores++;
+                            sensor_query = sensor_query + "_Luz";
+                        }
+
+                        sensor_query = cantSensores + sensor_query;
+
+                        ResourceManager rm = new ResourceManager("WumpusWorld.ImagenesRes", Assembly.GetExecutingAssembly());
+                        cueva[x, y].BackgroundImage = rm.GetObject("_" + sensor_query) as Image;
+                        cueva[x, y].BackgroundImageLayout = ImageLayout.Stretch;
+                        rm.ReleaseAllResources();
                     }
-                    else if (posOro[x, y])
-                    {
-                        cueva[x, y].BackColor = Color.Yellow;
-                        if (x + 1 <= 3)
-                            sensor_resplandor[x + 1, y] = true;
-                        if (x - 1 >= 0)
-                            sensor_resplandor[x - 1, y] = true;
-                        if (y + 1 <= 3)
-                            sensor_resplandor[x, y + 1] = true;
-                        if (y - 1 >= 0)
-                            sensor_resplandor[x, y - 1] = true;
-                    }
-                    else if (posSalida[x, y])
-                    {
-                        cueva[x, y].BackColor = Color.Brown;
-                        if (x + 1 <= 3)
-                            sensor_luz[x + 1, y] = true;
-                        if (x - 1 >= 0)
-                            sensor_luz[x - 1, y] = true;
-                        if (y + 1 <= 3)
-                            sensor_luz[x, y + 1] = true;
-                        if (y - 1 >= 0)
-                            sensor_luz[x, y - 1] = true;
-                    }
-                    else if (posHoyos[x, y])
-                    {
-                        cueva[x, y].BackColor = Color.Black;
-                        if (x + 1 <= 3)
-                            sensor_brisa[x + 1, y] = true;
-                        if (x - 1 >= 0)
-                            sensor_brisa[x - 1, y] = true;
-                        if (y + 1 <= 3)
-                            sensor_brisa[x, y + 1] = true;
-                        if (y - 1 >= 0)
-                            sensor_brisa[x, y - 1] = true;
-                    }
+
 
                     this.Controls.Add(cueva[x, y]);
                 }
@@ -260,11 +314,97 @@ namespace WumpusWorld
 
         private void ActivarSensores()
         {
-            brisaDetector.BackColor = sensor_brisa[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.Red : Control.DefaultBackColor;
-            hedorDetector.BackColor = sensor_hedor[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.Red : Control.DefaultBackColor;
-            resplandorDetector.BackColor = sensor_resplandor[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.Yellow : Control.DefaultBackColor;
-            gritoDetector.BackColor = sensor_grito[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.Green : Control.DefaultBackColor;
-            luzDetector.BackColor = sensor_luz[PosX_Actual_Jugador, PosY_Actual_Jugador] ? Color.SkyBlue : Control.DefaultBackColor;
+            int cantSensores = 1;
+            string sensor_query = "_J";
+            if(sensor_brisa[PosX_Actual_Jugador, PosY_Actual_Jugador])
+            {
+                cantSensores++;
+                brisaDetector.BackColor = Color.Red;
+                sensor_query = sensor_query + "_Brisa";
+            }
+            else
+            {
+                brisaDetector.BackColor = Control.DefaultBackColor;
+            }
+            if (sensor_hedor[PosX_Actual_Jugador, PosY_Actual_Jugador])
+            {
+                cantSensores++;
+                hedorDetector.BackColor = Color.Red;
+                sensor_query = sensor_query + "_Hedor";
+            }
+            else
+            {
+                hedorDetector.BackColor = Control.DefaultBackColor;
+            }
+            if (sensor_resplandor[PosX_Actual_Jugador, PosY_Actual_Jugador])
+            {
+                cantSensores++;
+                resplandorDetector.BackColor = Color.Green;
+                sensor_query = sensor_query + "_Brillo";
+            }
+            else
+            {
+
+                resplandorDetector.BackColor = Control.DefaultBackColor;
+            }
+            if (sensor_luz[PosX_Actual_Jugador, PosY_Actual_Jugador])
+            {
+                cantSensores++;
+                luzDetector.BackColor = Color.Green;
+                sensor_query = sensor_query + "_Luz";
+            }
+            else
+            {
+                luzDetector.BackColor = Control.DefaultBackColor;
+            }
+
+            sensor_query = cantSensores + sensor_query;
+
+            ResourceManager rm = new ResourceManager("WumpusWorld.ImagenesRes", Assembly.GetExecutingAssembly());
+            cueva[PosX_Actual_Jugador, PosY_Actual_Jugador].BackgroundImage = rm.GetObject("_" + sensor_query) as Image;
+            rm.ReleaseAllResources();
+
+            if (posWumpus[PosX_Actual_Jugador, PosY_Actual_Jugador])
+            {
+                cueva[PosX_Actual_Jugador, PosY_Actual_Jugador].BackgroundImage = WumpusWorld.ImagenesRes.wumpus;
+                MessageBox.Show("Fuite comido por el wumpus.... Perdiste");
+            }
+
+            if (posHoyos[PosX_Actual_Jugador, PosY_Actual_Jugador])
+            {
+                cueva[PosX_Actual_Jugador, PosY_Actual_Jugador].BackgroundImage = WumpusWorld.ImagenesRes.hoyo;
+                MessageBox.Show("Caiste en un hoyo.... Perdiste");
+            }
+
+            if (posSalida[PosX_Actual_Jugador, PosY_Actual_Jugador])
+            {
+                cueva[PosX_Actual_Jugador, PosY_Actual_Jugador].BackgroundImage = WumpusWorld.ImagenesRes.puerta;
+                MessageBox.Show("Encontraste la salida.... Ganaste");
+            }
+
+            if (posOro[PosX_Actual_Jugador, PosY_Actual_Jugador])
+            {
+                cueva[PosX_Actual_Jugador, PosY_Actual_Jugador].BackgroundImage = WumpusWorld.ImagenesRes.oro;
+                flechas[cantFlechas] = new PictureBox();
+                flechas[cantFlechas].Size = new Size(150, 150);
+                flechas[cantFlechas].Location = new Point((cantFlechas) * 150 + 40, 50);
+                flechas[cantFlechas].BackgroundImage = WumpusWorld.ImagenesRes.flecha;
+                flechas[cantFlechas].BackgroundImageLayout = ImageLayout.Stretch;
+                flechas[cantFlechas].Click += CargarArco;
+                flechasGroup.Controls.Add(flechas[cantFlechas]);
+                cantFlechas++;
+                MessageBox.Show("Encontraste el ORO... + 1 flecha, que suerte!");
+                posOro[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
+                if (PosX_Actual_Jugador + 1 <= 3)
+                    sensor_resplandor[PosX_Actual_Jugador + 1, PosY_Actual_Jugador] = false;
+                if (PosX_Actual_Jugador - 1 >= 0)
+                    sensor_resplandor[PosX_Actual_Jugador - 1, PosY_Actual_Jugador] = false;
+                if (PosY_Actual_Jugador + 1 <= 3)
+                    sensor_resplandor[PosX_Actual_Jugador, PosY_Actual_Jugador + 1] = false;
+                if (PosY_Actual_Jugador - 1 >= 0)
+                    sensor_resplandor[PosX_Actual_Jugador, PosY_Actual_Jugador - 1] = false;
+
+            }
         }
 
         private void btnDerecha_Click(object sender, EventArgs e)
@@ -272,6 +412,7 @@ namespace WumpusWorld
             posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
             PosX_Actual_Jugador = PosX_Actual_Jugador + 1;
             posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
+            recorrido[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
             redefinirCueva();
             definirMovimiento();
             ActivarSensores();
@@ -282,6 +423,7 @@ namespace WumpusWorld
             posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
             PosX_Actual_Jugador = PosX_Actual_Jugador - 1;
             posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
+            recorrido[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
             redefinirCueva();
             definirMovimiento();
             ActivarSensores();
@@ -292,6 +434,7 @@ namespace WumpusWorld
             posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
             PosY_Actual_Jugador = PosY_Actual_Jugador - 1;
             posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
+            recorrido[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
             redefinirCueva();
             definirMovimiento();
             ActivarSensores();
@@ -302,9 +445,12 @@ namespace WumpusWorld
             posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = false;
             PosY_Actual_Jugador = PosY_Actual_Jugador + 1;
             posJugador[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
+            recorrido[PosX_Actual_Jugador, PosY_Actual_Jugador] = true;
             redefinirCueva();
             definirMovimiento();
             ActivarSensores();
         }
+
+
     }
 }
